4e38ed361ea36a5d52afd07e8ed8393b1a9fa2f8
Kostya Bilomazur <k.bilomazur@lionwood.software>
2021-12-20 10:32:11 +0200

    Initial Commit

---
+++ Sources/CalendarContol.xcframework/ios-arm64/CalendarContol.framework/Headers/CalendarContol.h
@@ -0,0 +1,18 @@
+//
+//  CalendarContol.h
+//  CalendarContol
+//
+//  Created by Kostya Bilomazur on 17.12.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CalendarContol.
+FOUNDATION_EXPORT double CalendarContolVersionNumber;
+
+//! Project version string for CalendarContol.
+FOUNDATION_EXPORT const unsigned char CalendarContolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarContol/PublicHeader.h>
+
+
